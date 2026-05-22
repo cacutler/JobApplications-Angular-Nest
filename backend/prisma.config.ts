6c7fa2f9cfa7d97dddab25a9adafafece2bd5d65
Alex Cutler <calexcutler@gmail.com>
2026-05-21 18:26:52 -0600

    Updated some code styling

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,8 +1,3 @@
 import "dotenv/config";
 import {defineConfig, env} from "@prisma/config";
-export default defineConfig({
-  schema: "prisma/schema.prisma",
-  datasource: {
-    url: env("DATABASE_URL"), 
-  },
-});
+export default defineConfig({schema: "prisma/schema.prisma", datasource: {url: env("DATABASE_URL")}});
