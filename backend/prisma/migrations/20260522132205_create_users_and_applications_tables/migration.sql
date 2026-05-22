84e0017f9e03f84628f855e0955c8ba552c1b4a5
Alex Cutler <calexcutler@gmail.com>
2026-05-22 07:28:10 -0600

    Updated prisma schema to map some strings to database text types and migrated the database

---
+++ backend/prisma/migrations/20260522132205_create_users_and_applications_tables/migration.sql
@@ -0,0 +1,30 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `username` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `User_username_key`(`username`),
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Application` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `userId` INTEGER NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NULL,
+    `qualifications` VARCHAR(191) NULL,
+    `status` ENUM('APPLIED', 'REVIEW', 'APPROVED', 'REJECTED') NOT NULL DEFAULT 'APPLIED',
+    `stage` VARCHAR(191) NULL,
+    `notes` VARCHAR(191) NULL,
+    `submission` DATETIME(3) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Application` ADD CONSTRAINT `Application_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ backend/prisma/migrations/20260522132534_change_description_qualifications_and_notes_types_to_text/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE `Application` MODIFY `description` TEXT NULL,
+    MODIFY `qualifications` TEXT NULL,
+    MODIFY `notes` TEXT NULL;
