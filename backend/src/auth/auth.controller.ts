d789942bcaf8cad027d104903462d7142360674b
Alex Cutler <calexcutler@gmail.com>
2026-05-27 16:39:37 -0600

    Finished auth logic implementation

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,3 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Get, UseGuards, Request } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from '../users/dto/create-user.dto';
+import { LoginDto } from './dto/login.dto';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService) {}
+    @Post('register')
+    register(@Body() dto: CreateUserDto) {
+        return this.authService.register(dto);
+    }
+    @Post('login')
+    login(@Body() dto: LoginDto) {
+        return this.authService.login(dto);
+    }
+    @UseGuards(JwtAuthGuard)
+    @Get('me')
+    getMet(@Request() req) {
+        return req.user;
+    }
+}
