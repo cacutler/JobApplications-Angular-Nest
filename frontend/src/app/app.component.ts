3f917c904b08eb94186735f65219ff1843fd5fab
Alex Cutler <calexcutler@gmail.com>
2026-05-28 20:54:15 -0600

    Updated code styling

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
-})
+@Component({selector: 'app-root', imports: [RouterOutlet], templateUrl: './app.component.html', styleUrl: './app.component.css'})
 export class AppComponent {
   title = 'frontend';
 }
